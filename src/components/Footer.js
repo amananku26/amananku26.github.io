66470670c2a240f5e7d11b6ba47dd0a4338212db
AMAN ANKU <amankumaranku@gmail.com>
2021-02-26 18:38:14 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -49,6 +49,8 @@ const Footer = () => {
               >
                 <img height="25px" src="https://cdn3.iconfinder.com/data/icons/2018-social-media-logotypes/1000/2018_social_media_popular_app_logo_youtube-512.png" />
               </Nav.Link>
+
+              
             </div>
           </Col>
           <Col md={5} xs={12}>
