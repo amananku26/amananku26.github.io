c0c5dc27f9b3bda2b03323a0d0b840648fac497f
AMAN ANKU <amankumaranku@gmail.com>
2021-03-02 22:18:35 +0530

    update scroll

---
+++ src/Scroll.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import {FaArrowCircleUp} from 'react-icons/fa';
+import './scroll.css';
+
+
+const ScrollArrow = () =>{
+
+  const [showScroll, setShowScroll] = useState(false)
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400){
+      setShowScroll(true)
+    } else if (showScroll && window.pageYOffset <= 400){
+      setShowScroll(false)
+    }
+  };
+
+  const scrollTop = () =>{
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  };
+
+  window.addEventListener('scroll', checkScrollTop)
+
+  return (
+        <FaArrowCircleUp className="scrollTop" onClick={scrollTop} style={{height: 40, display: showScroll ? 'flex' : 'none'}}/>
+  );
+}
+
+export default ScrollArrow;
