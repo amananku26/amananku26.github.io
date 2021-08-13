75e7b12ee2d5c65e50e0b742ece23e8e01d85d47
amananku26 <amankumaranku@gmail.com>
2021-08-13 09:35:41 +0530

    stage=1

--- src/index.js
+++ src/index.js
@@ -4,20 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-  // sentry errors
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
-  
-  Sentry.init({
-    dsn: "https://48d1238af23849edb89e1f2e6ea4435d@o624997.ingest.sentry.io/5753626",
-    integrations: [new Integrations.BrowserTracing()],
-  
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
