4bb5a03a0e9e4630ae6142d55639e6f39d9cf073
amananku26 <amankumaranku@gmail.com>
2021-11-05 17:27:38 +0530

    imporved seo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import "./App.css";
+import {Component} from "react"
 import "bootstrap/dist/css/bootstrap.min.css";
 import Feed from "./components/homePage/Feedback"
 import HomePage from "./components/homePage/HomePage";
 import ScrollArrow from "./Scroll.jsx"
-function App() {
+import Tour from 'reactour'
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      isTourOpen : true,
+      isShowingMore: false
+    }
+
+  }
+  closeTour = () =>{
+    this.setState({ isTourOpen: false });
+  }
+  toggleShowMore = () => {
+    this.setState((prevState) => ({
+      isShowingMore: !prevState.isShowingMore
+    }));
+  };
+  render(){
   return (
     <div className="App">
       <HomePage />
-      <Feed/>
+      <Feed 
+      data-tut="reactour__copy"
+      dddd = {this.state.isTourOpen}
+      openTour={this.openTour}
+      toggleShowMore={this.toggleShowMore}
+      isShowingMore={this.state.isShowingMore}
+      />
       <ScrollArrow/>
+      <Tour
+       steps={steps}
+       isOpen={this.state.isTourOpen}
+      //  maskClassName="mask"
+       className="helper"
+        rounded={5}
+       onRequestClose={this.closeTour}/>;
     </div>
   );
 }
+}
+const steps = [
+  {
+    selector: '[data-tut="reactour__copy"]',
+    content: 'Please Give Feedback it will hardly take 30 sec (Bottom Right Side Corner of Screen )',
+  }
+]
 
 export default App;
--- src/components/homePage/Feedback.js
+++ src/components/homePage/Feedback.js
@@ -5,22 +5,23 @@ import "./Feeder.css"
 
 class Feed extends Component {
   render() {
-    return <Feedback projectId="6028a50ea0752f0004fb743a" />;
+    const { openTour, isShowingMore, toggleShowMore,dddd } = this.props
+    Feedback.defaultProps = {
+      email: true,
+      emailRequired: true,
+      emailDefaultValue: "",
+      projectName: "",
+      subProject: "",
+      primaryColor: dddd ? 'Yellow' :'white',
+      textColor: "black",
+      hoverBorderColor: "#000000",
+      postSubmitButtonMsg: "Thanks!",
+      submitButtonMsg: "Send Feedback",
+      feedbackTypes: ["general", "bug", "idea"],
+      zIndex: "100000000"
+    };
+    return <span><Feedback projectId="6028a50ea0752f0004fb743a" classname = 'frf-feedback-container'/></span>;
   }
 }
-Feedback.defaultProps = {
-    email: true,
-    emailRequired: true,
-    emailDefaultValue: "",
-    projectName: "",
-    subProject: "",
-    primaryColor: "white",
-    textColor: "black",
-    hoverBorderColor: "#000000",
-    postSubmitButtonMsg: "Thanks!",
-    submitButtonMsg: "Send Feedback",
-    feedbackTypes: ["general", "bug", "idea"],
-    zIndex: "100000000"
-  };
 
 export default Feed
