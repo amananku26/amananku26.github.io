716b253d3a9430f7f3bcdb5ef50682e6b9359ec5
Aman Anku <aman.anku@nextory.com>
2022-08-08 15:18:35 +0530

    redirect to linktree

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ class App extends Component {
     }
 
   }
+
   componentDidMount(){
+  window.location.href = 'https://linktr.ee/amananku26'  
   this.setState({isLoading:false})  
   const getFromFirebase = firebase.firestore().collection("count");
   getFromFirebase.onSnapshot((querySnapShot) => {
