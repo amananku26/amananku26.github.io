6345d3ec06f4f2efa2cbdad47a99111e3e17d8b8
amananku26 <amankumaranku@gmail.com>
2026-08-15 18:12:47 +0530

    Add server-side feedback API and related manifest files

---
+++ .next/dev/server/middleware-build-manifest.js
@@ -0,0 +1,15 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [],
+  "lowPriorityFiles": [
+    "static/development/_buildManifest.js",
+    "static/development/_ssgManifest.js",
+    "static/development/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [],
+  "rootMainFilesTree": {},
+  "pagesChunkGroupBootstrapParams": {}
+};
---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CrWDrHTw1qnsRo+M/x3Xvc77mWonFFMRNXew/onxDjM=\"\n}"
